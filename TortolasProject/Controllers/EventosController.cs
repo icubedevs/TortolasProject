9de00cbed6c69f392982c96e09e3e7d99f3bf89c
mostroh <belakor.l2@gmail.com>
2012-04-07 19:35:52 +0200

    pufff que coñazo

---
+++ TortolasProject/Controllers/EventosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TortolasProject.Models;
+
+namespace TortolasProject.Controllers
+{
+    public class EventosController : Controller
+    {
+        //
+        // GET: /Eventos/
+        mtbMalagaDataContext mtbMalagaDB = new mtbMalagaDataContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
