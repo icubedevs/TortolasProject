c9ee1d0b25d5947615a5cf209a0df6d3ab7aa973
Prozid <daniprolongo@hotmail.com>
2012-04-13 09:53:42 +0200

    Añadido repositorio de facturas.

--- TortolasProject/Controllers/FacturasController.cs
+++ TortolasProject/Controllers/FacturasController.cs
@@ -4,19 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TortolasProject.Models;
+using TortolasProject.Models.Repositorios;
 
 namespace TortolasProject.Controllers
 {
     public class FacturasController : Controller
     {
-        // Conecto a la BD
-        mtbMalagaDataContext mtbMalagaDB = new mtbMalagaDataContext();
-
+        static FacturasRepositorio FacturasRepo = new FacturasRepositorio();
        
         // Index
         public ActionResult Index()
         {
-            var facturas = from estadoFactura in mtbMalagaDB.tbEstadoFactura select estadoFactura;
+            //var facturas = from estadoFactura in FacturasRepositoriodoFactura select estadoFactura;
         
             return View();
         }
@@ -24,22 +23,58 @@ namespace TortolasProject.Controllers
         
         public ActionResult Create()
         {
-            return PartialView();
+            return PartialView("Create");
         }
-
-
-        //Prueba de AJAX
+        
+        //LeerFacturas
         [HttpPost]
-        public JsonResult Prueba(string n, string c)
+        public ActionResult leerTodos()
         {
-            string nombre = n + "Prozid";
-            string coche = c + " 16V eco2";
-
-            String [] array = {nombre, coche}; 
+            var usuarios = from u in FacturasRepo.listarTodos() 
+                           select new
+                               {
+                                   idUsuario = u.idUsuario,
+                                   Nickname = u.Nickname,
+                                   DNI = u.DNI,
+                                   Email = u.Email
+                               };
             
 
-            return Json(array);
-        }               
+            return Json(usuarios);
+        }
 
+        public class usuarioPrueba
+        {
+            public Guid idUsuario { get; set;}
+            public String Nickname { get; set;}
+            public String DNI { get; set;}
+            public String Email { get; set;}
+        }
+
+        [HttpPost]
+        public ActionResult leerPrueba()
+        {
+            List<usuarioPrueba> listaUsuarios = new List<usuarioPrueba>();
+            
+            usuarioPrueba dani = new usuarioPrueba{
+                idUsuario = new Guid(),
+                Nickname = "dani",
+                Email = "dani@dani.com",
+                DNI = "123456789"
+            };
+
+            usuarioPrueba ismi = new usuarioPrueba
+            {
+                idUsuario = new Guid(),
+                Nickname = "ismi",
+                Email = "ismi@dani.com",
+                DNI = "123456389"
+            };
+
+            listaUsuarios.Add(dani);
+            listaUsuarios.Add(ismi);
+
+            return Json(listaUsuarios);           
+        }
     }
 }
---
+++ TortolasProject/Models/Repositorios/FacturasRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TortolasProject.Models.Repositorios
+{
+    public class FacturasRepositorio
+    {
+        // Conecto a la BD
+        mtbMalagaDataContext mtbMalagaDB = new mtbMalagaDataContext();
+
+        public IList<tbUsuario> listarTodos()
+        {
+            return mtbMalagaDB.tbUsuario.ToList();
+
+        }
+    }
+}
