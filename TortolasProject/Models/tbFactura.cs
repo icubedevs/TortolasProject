0833f246fd59b379629417918b818ad34808cf6f
Prozid <daniprolongo@hotmail.com>
2012-05-12 01:07:50 +0200

    tbFactura.cs: Inclusión de propiedades necesarias para el control de relaciones con otros subsistemas desde la vista.

---
+++ TortolasProject/Models/tbFactura.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TortolasProject.Models
+{
+    public partial class tbFactura
+    {
+        public string vista { get; set; }
+        public string idRelacion { get; set; }
+        public string tipo { get; set; }
+    }
+}
