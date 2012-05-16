0c26b6d860799eb148aab3c4f2755d4f3d53d8ea
Prozid <daniprolongo@hotmail.com>
2012-05-16 18:46:18 +0200

    Movimiento.cs: Clase que "une" las tablas MovimientoGasto y MovimientoIngreso.

---
+++ TortolasProject/Models/Movimiento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TortolasProject.Models
+{
+    public class Movimiento
+    {
+        public Guid idMovimiento { get; set; }
+        public String Concepto { get; set; }
+        public String Descripcion { get; set; }
+        public DateTime Fecha { get; set; }
+        public Guid Responsable { get; set; }
+        public String ResponsableName { get; set; }
+        public Guid FKFactura { get; set; }
+        public Decimal Total { get; set; }
+    }
+}
