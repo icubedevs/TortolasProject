b52c92da12a9850455a7ca16308994b15551574f
Prozid <daniprolongo@hotmail.com>
2012-05-18 10:27:07 +0200

    facturasNav.js: Limpieza de código.

--- TortolasProject/Scripts/jsactions/Facturas/facturasNav.js
+++ TortolasProject/Scripts/jsactions/Facturas/facturasNav.js
@@ -1,51 +1,5 @@
 ﻿$(document).ready(function () {
 
-    $("#volverFacturasNav").hide();
 
-    $("#volverFacturasNav").click(function () {
-        volverListaFacturas();
-    });
-
-    /*
-    $("#nuevaFacturaButtonNav").click(function () {
-        $.post('Facturas/cargarVistaNuevaFactura', function (data) {
-            $("#FacturasGrid").hide();
-            $("#FacturasContainer").html(data);
-            $("#FacturasContainer").show();
-            $("#mainFacturasNav").hide();
-            $("#volverFacturasNav").show();
-        });
-    }); */
-
-    $("#ingresosButtonNav").click(function () {
-
-        url = 'Facturas/nuevaFactura';
-        datos = { name: "dani", coche: "twingo" }
-        $.post(url, datos, function (data) {
-            $("#FacturasGrid").fadeOut('slow', function () {
-                $(this).hide();
-                $("#FacturasContainer").html(data);
-            });
-        });
-    });
-
-    $("#gastosButtonNav").click(function () {
-        $.post('Facturas/Details', function (data) {
-            $("#FacturasContainer").html(data);
-            $("#FacturasIndexGrid").hide();
-        });
-    });
-
-    $("#graficosContablesButtonNav").click(function () {
-        $("#FacturasContainer").html("Graficos contables view");
-    });
+   
 });
-
-function volverListaFacturas() {
-    $("#volverFacturasNav").hide();
-    $("#FacturasContainer").hide();
-    $("#FacturasGrid").data("kendoGrid").dataSource.read();
-    $("#FacturasGrid").show();
-    $("#mainFacturasNav").show();
-
-}
